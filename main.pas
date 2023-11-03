eb1ce150fd5d5d35d6c3e906fe415fbb7db0c26f
Connor Brady <connorbrady@codox.eu>
2023-11-03 07:14:32 +0000

    WIP Adding folder select

--- main.pas
+++ main.pas
@@ -5,10 +5,20 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
+
+  { TMainForm }
+
   TMainForm = class(TForm)
+    ButtonSelectFolder: TButton;
+    LabelTitle: TLabel;
+    SelectDirectoryDialog1: TSelectDirectoryDialog;
+    procedure ButtonSelectFolderClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure LabelSelectFileClick(Sender: TObject);
+    procedure LabelTitleClick(Sender: TObject);
   private
 
   public
@@ -21,6 +31,28 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.ButtonSelectFolderClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.LabelSelectFileClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.LabelTitleClick(Sender: TObject);
+begin
+
+end;
 
 end.
 
